ae10cde279aa1c7652f391862af178371fc37cd3
Frederik Anspach <frederik.anspach@web.de>
2026-02-10 22:55:52 +0100

    add joke fetching logic and auto-load on start

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
 import "./style.scss";
 
+// const buttonSaveJoke = document.querySelector("#save-joke");
+// const savedJokesList = document.querySelector("saved-jokes__list");
+// let savedJokes = JSON.parse(localStorage.getItem("myJokes")) || [];
+
+const jokeUrl = "https://witzapi.de/api/joke?language=de";
 const jokeTextDisplay = document.querySelector(".current-joke__text");
 const buttonNewJoke = document.querySelector("#new-joke");
-const buttonSaveJoke = document.querySelector("#save-joke");
-const savedJokesList = document.querySelector("saved-jokes__list");
+let newJoke;
 
-let savedJokes = JSON.parse(localStorage.getItem("myJokes")) || [];
+async function getJoke(jokeUrl) {
+  console.log("Hole Joke von: " + jokeUrl);
 
-// TEST //
+  try {
+    const response = await fetch(jokeUrl);
+
+    if (!response.ok) {
+      throw new Error(`Server-Fehler: ${response.status}`);
+    }
+
+    const body = await response.json();
+    return body[0].text;
+  } catch (error) {
+    console.log(error.message);
+    return "Ups! Kein Witz verfügbar. Prüfe deine Verbindung.";
+  }
+}
+
+function appendJoke(newJoke) {
+  jokeTextDisplay.textContent = newJoke;
+}
+
+async function handleNewJokeClick() {
+  newJoke = await getJoke(jokeUrl);
+  appendJoke(newJoke);
+}
+
+buttonNewJoke.addEventListener("click", handleNewJokeClick);
+handleNewJokeClick();
