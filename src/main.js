06c5e5cc5df256bef3ad8397e72c66ded17c907f
Frederik Anspach <frederik.anspach@web.de>
2026-02-12 23:44:30 +0100

    add theme toggle and storage cleanup

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import "./style.scss";
 import { FavoriteJokes } from "./FavoriteJokes.js";
 
 const JOKE_URL = "https://witzapi.de/api/joke?language=de";
+let newJoke;
+
 const jokeTextDisplay = document.querySelector(".current-joke__text");
 const buttonNewJoke = document.querySelector("#new-joke");
 const buttonSaveJoke = document.querySelector("#save-joke");
-let newJoke;
+const buttonTheme = document.querySelector("#button-theme");
+const buttonDeleteStorage = document.querySelector("#delete-storage");
 
 async function fetchJoke(jokeUrl) {
   try {
@@ -54,9 +57,9 @@ function displayFavoriteJokes() {
     listItem.innerHTML = `
       <p class="saved-jokes__item-text">${joke}</p>
       <button class="saved-jokes__delete-icon" aria-label="Witz löschen">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
+      </svg>
       </button>
     `;
 
@@ -80,6 +83,28 @@ function handleSaveJokeClick() {
 
   displayFavoriteJokes();
 }
+
+if (localStorage.getItem("theme") == "light") {
+  document.body.classList.add("light-mode");
+}
+
+buttonTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+buttonDeleteStorage.addEventListener("click", () => {
+  if (confirm("Möchtest du alle Witze und Einstellungen löschen?")) {
+    localStorage.clear();
+    displayFavoriteJokes();
+    document.body.classList.remove("light-mode");
+  }
+});
 
 buttonNewJoke.addEventListener("click", handleNewJokeClick);
 buttonSaveJoke.addEventListener("click", handleSaveJokeClick);
